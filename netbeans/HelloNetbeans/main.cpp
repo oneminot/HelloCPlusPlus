be3f562c43b8bc523252ac299cf94ef9ae66f689
kushal <kushaldeveloper@gmail.com>
2014-11-08 12:33:45 -0600

    hello netbeans

---
+++ netbeans/HelloNetbeans/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: toshiba
+ *
+ * Created on November 8, 2014, 12:30 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
